90ff20dd69e80a18621661de41acb45115edc470
Heo JaeYeong <likeblue74@gmail.com>
2020-06-07 15:57:48 +0900

    오타수정

--- building.py
+++ building.py
@@ -689,7 +689,7 @@ class Building(object):
             if floor_passenger[i] > 0:
                 dest_list = self.floors[i].add_passenger(floor_passenger[i])
                 self.add_passenger += floor_passenger[i]
-                passengself.rest_er -= floor_passenger[i]
+                self.rest_passenger -= floor_passenger[i]
 
     def take_passenger(self):
         for floor in self.floors:
